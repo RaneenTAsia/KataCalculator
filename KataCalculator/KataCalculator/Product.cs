a93695aea02f334d686664e77c6885491de28f65
Raneen Asia <Raneenasia101@gmail.com>
2023-02-05 12:03:30 +0200

    Add First Task Files

---
+++ KataCalculator/KataCalculator/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCalculator
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int UPC { get; set; }
+        public decimal BasePrice { get; set; }
+        public decimal TaxPrice { get; private set; }
+        decimal _taxPercent = 20M;
+
+        public decimal TaxPercent
+        {
+            get { return _taxPercent; }
+            set
+            { 
+                _taxPercent = value;
+                TaxPrice = CalculateTaxPrice();
+            }
+        }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, int UPC, decimal Price)
+        {
+            Name = name;
+            this.UPC = UPC;
+            this.BasePrice = Price;
+            TaxPrice= CalculateTaxPrice();
+        }
+       public decimal CalculateTaxValue()
+        {
+            return Math.Round(this.BasePrice * (TaxPercent / 100), 2);
+        }
+
+        private decimal CalculateTaxPrice()
+        {
+            return CalculateTaxValue()+this.BasePrice;
+        }
+
+        override public string ToString()
+        {
+            USDollar currency = new USDollar(BasePrice);
+            return $"Name: {Name}, UPC: {UPC}, Price: {currency.ToString}";
+        }
+    }
+}
--- KataCalculator/KataCalculator/Program.cs
+++ KataCalculator/KataCalculator/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using KataCalculator;
+
+ProductViewModel view = new ProductViewModel();
+
+PrintProducts(view);
+
+static void PrintProducts(ProductViewModel view)
+{
+    view.GetAll();
+    foreach (var item in view.Products)
+    {
+        Console.WriteLine(item.ToString());
+        Console.WriteLine($"Product price reported as ${item.BasePrice} before tax and ${item.CalculateTaxValue()} after {item.TaxPercent}% tax.");
+        item.TaxPercent = 30M;
+        Console.WriteLine(item.ToString());
+        Console.WriteLine($"Product price reported as ${item.BasePrice} before tax and ${item.TaxPrice} after {item.TaxPercent}% tax.");
+        Console.WriteLine();
+    }
+}
+
